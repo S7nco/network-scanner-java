3b41939a64e1b83189808cdc14174c3be546a014
S7nco <tip.pope-0u@icloud.com>
2026-04-01 16:54:03 -0500

    Adjusted to how the result output was printed. Created an array to contain the found hosts and print the array at the end of the search.

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,30 @@
 //look professional
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         String baseIP = "192.168.1."; //Change to correct network
+        ArrayList<String> reachableHosts = new ArrayList<>();
         System.out.println("Scanning network: " + baseIP + "1-254");
 
         for (int i = 1; i <= 254; i++) {
             String host = baseIP + i;
+            System.out.println("Checking: " + host); //This is so I could tell it was scanning the network
             try {
                 InetAddress address = InetAddress.getByName(host);
                 if (address.isReachable(2000)) { // 2 second timeout
-                    System.out.println(host + " is reachable");
+                    reachableHosts.add(host);
                 }
             } catch (Exception e) {
                 System.out.println(host + " is not reachable");
             }
+        }
+        System.out.println("\nScan Complete");
+        System.out.println("Reachable Hosts:");
+        for (String ip : reachableHosts) {
+            System.out.println("- " + ip);
         }
     }
 }
